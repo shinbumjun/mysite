1056659813ba5f1406527bb50927fd42916343ac
shinbumjun <https://github.com/shinbumjun>
2022-05-27 03:57:39 +0900

    git 설치, git 사용법, github 사용법

---
+++ mysite/urls.py
@@ -0,0 +1,23 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('polls/', include('polls.urls')), # http://127.0.0.1:8000/polls/
+    # 우리가 작성한 url연결 (../polls을 파싱해서 해당 path를 잡아내고 polls.urls로 연결해 주는것)
+    path('admin/', admin.site.urls),
+]
