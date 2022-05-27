68864de36f07c8b02be6d5ca421e1572a0f938b6
shinbumjun <https://github.com/shinbumjun>
2022-05-27 09:58:28 +0900

    admin 사용법

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')), # http://127.0.0.1:8000/polls/
     # 우리가 작성한 url연결 (../polls을 파싱해서 해당 path를 잡아내고 polls.urls로 연결해 주는것)
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # 관리자 전용 페이지 : http://127.0.0.1:8000/admin/
 ]
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
+
+admin.site.register(Question) # 모델중 하나 Question을 admin에 등록
