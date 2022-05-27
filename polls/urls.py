9e196ae5d759a24e68c4a1690350ca7a1b1438bb
shinbumjun <https://github.com/shinbumjun>
2022-05-27 17:00:54 +0900

    django form 사용, 데이터 받아오는 법, generic view 사용

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,19 @@ from . import views
 app_name = 'polls' # 이름명시
 urlpatterns = [
     # ex: /polls/ -> index (클라이언트가 polls라고 호출을 하게 되면 패턴대로 아무것도 없으니까 views.index라는 views를 호출)
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'), # 함수기반 
+    path('', views.IndexView.as_view(), name='index'), # class기반 view 사용
+
     # ex: /polls/5/ : 상세페이지를 클라이언트에 전달 
-    path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/', views.detail, name='detail'), 
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
     # ex: /polls/5/vote/ : url패턴
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # pk란? DB내의 하나의 열, 하나의 데이터를 구분할 수 있는 값, pk값은 중복x
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,57 +1,106 @@
 from django.shortcuts import render # 지름길 : render() 함수
-from django.http import HttpResponse, Http404 # 에러
+from django.http import HttpResponse, HttpResponseRedirect, Http404 # 에러
 from django.template import loader
-from .models import Question
+from .models import Choice, Question
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-# Create your views here.
-def index(request): # 정리 : urls.py -> views.py
-    # 1
-    # return HttpResponse("Hello, World.") # 응답
+from django.views import generic
 
-    # 2
-    # latest_question_list = Question.objects.order_by('-pub_data')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    # Question 데이터 중에서 출판 일자를 정렬해서 5개 까지만 데이터를 가지고오고 ,로 연결
+# class기반(제너릭) view 사용하기
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    # 3
-    # latest_question_list = Question.objects.order_by('-pub_data')[:5]
-    # template = loader.get_template('polls/index.html') # 3. html로 
-    # context = { # 1. context 안에 데이터를 밀어 넣어주고 
-    #     'latest_question_list': latest_question_list, # 2. 데이터를 template에 전달
-    # }
-    # return HttpResponse(template.render(context, request))
-    
-    # 4
-    latest_question_list = Question.objects.order_by('-pub_data')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_data')[:5]
 
-def detail(request, question_id): # 상세페이지
-    # 1
-    # return HttpResponse("You're looking at question %s." % question_id)
 
-    # 2
-    try:
-        question = Question.objects.get(pk=question_id) # question_id 전달을 받고 조회를 했을때
-    except Question.DoesNotExist: # 데이터가 없는 경우 에러
-        raise Http404("Question does not exist : 해당 데이터가 없습니다") # text 전달
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    # 3 (이건 이해못함)
-    # def detail(request, question_id):
-    #     question = get_object_or_404(Question, pk=question_id)
-    #     return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id): # ...
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def vote(request, question_id): # url패턴
-    return HttpResponse("You're voting on question %s." % question_id)    
+    
+    # GET, POST방식 설명
+    # if request.method == "GET":
+    #     do_somthing()
+    # elif request.method == "POST":
 
-# 설명 : request라는 인자를 받고 HttpResponse 함수를 리턴
-# 클라이언트로부터 request 받게 되면 request 여러가지 정보들이 담겨 있을 것이고 
-# HttpResponse 전에 데이터 추출, 저장, 파일 다운로드 등등을 작성하고
-# HttpResponse 해준다는것만 명심!!! 
+        question = get_object_or_404(Question, pk=question_id) # 조회
+
+        # return HttpResponse("You're voting on question %s." % question_id)    
+
+        # 설명 : request라는 인자를 받고 HttpResponse 함수를 리턴
+        # 클라이언트로부터 request 받게 되면 request 여러가지 정보들이 담겨 있을 것이고 
+        # HttpResponse 전에 데이터 추출, 저장, 파일 다운로드 등등을 작성하고
+        # HttpResponse 해준다는것만 명심!!! 
+        
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice']) # 외래키 조건(tmplates에서 넘겨받은 값을 조회)
+        except (KeyError, Choice.DoesNotExist): # 값이 없으면 예외발생
+            # Redisplay the question voting form.
+            return render(request, 'polls/detail.html', { # 데이터가 없는 경우 -> 상세페이지
+                'question': question,
+                'error_message': "아무것도 투표하지 않았습니다.", # 에러메시지
+            })
+        else: # 데이터가 있는 경우
+            selected_choice.votes += 1 # 선택지에 대한 votes를 1추가 
+            selected_choice.save()
+            
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # 결과 페이지를 보여줌 
+
+
+# 함수기반 views
+# def index(request): # 정리 : urls.py -> views.py
+#     # 1
+#     # return HttpResponse("Hello, World.") # 응답
+
+#     # 2
+#     # latest_question_list = Question.objects.order_by('-pub_data')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#     # Question 데이터 중에서 출판 일자를 정렬해서 5개 까지만 데이터를 가지고오고 ,로 연결
+
+#     # 3
+#     # latest_question_list = Question.objects.order_by('-pub_data')[:5]
+#     # template = loader.get_template('polls/index.html') # 3. html로 
+#     # context = { # 1. context 안에 데이터를 밀어 넣어주고 
+#     #     'latest_question_list': latest_question_list, # 2. 데이터를 template에 전달
+#     # }
+#     # return HttpResponse(template.render(context, request))
+    
+#     # 4
+#     latest_question_list = Question.objects.order_by('-pub_data')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id): # 상세페이지
+#     # 1
+#     # return HttpResponse("You're looking at question %s." % question_id)
+
+#     # 2
+#     try:
+#         question = Question.objects.get(pk=question_id) # question_id 전달을 받고 조회를 했을때
+#     except Question.DoesNotExist: # 데이터가 없는 경우 에러
+#         raise Http404("Question does not exist : 해당 데이터가 없습니다") # text 전달
+#     return render(request, 'polls/detail.html', {'question': question})
+
+#     # 3 (이건 이해못함)
+#     # def detail(request, question_id):
+#     #     question = get_object_or_404(Question, pk=question_id)
+#     #     return render(request, 'polls/detail.html', {'question': question})
+
+# def results(request, question_id): # ...
+#     # response = "You're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
