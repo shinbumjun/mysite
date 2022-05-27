d31872a42db1a531e60e92b20cd10005c4551f37
shinbumjun <https://github.com/shinbumjun>
2022-05-27 18:27:37 +0900

    customize admin 제외

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Question, Choice
-# Register your models here.
 
 admin.site.register(Question) # 모델중 하나 Question을 admin에 등록
 admin.site.register(Choice) # ...
+
